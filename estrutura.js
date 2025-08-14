39e0aa322842c705c8b831ca136e0be8098c4b5c
RafaelmachadoM <rafaelantunesmachadomariani@gmail.com>
2025-08-13 21:15:57 -0300

    upload

---
+++ estrutura.js
@@ -0,0 +1,17 @@
+
+function enviar(event){
+event.preventDefault()
+const nome = document.getElementById('nome').value;
+const mensagem = document.getElementById('mensagem').value;
+const telefone = '48991619091';
+
+const texto = `Olá Me chamo ${nome},${mensagem}`;
+const mensagemFormatada = encodeURIComponent(texto);
+
+const url = `https://wa.me/${telefone}?text=${mensagemFormatada}`
+
+window.open(url, '_blank');
+
+
+
+}
